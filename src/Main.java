196d5a05348818ab9a421bf10fdad2e0ffc9538e
Sokov Kirill <sokovkirill95@gmail.com>
2021-04-06 13:50:29 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int RUB = 13664;
+        int Miles = RUB / 20;
+        System.out.println("Миль зачислено: " + Miles);
+
+    }
+}
